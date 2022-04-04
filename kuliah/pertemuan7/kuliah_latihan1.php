ec1edbe83ca3a12e860ab8fb2179be56acee0b96
vinaanurfauziah <vinaanurfauziah@gmail.com>
2022-04-04 10:07:37 +0700

    menyelesaikan tugas pertemuan 7

---
+++ kuliah/pertemuan7/kuliah_latihan1.php
@@ -0,0 +1,46 @@
+<?php
+    // variabel global
+    // $nama = "Vina";
+    
+    // function cetakNama(){
+    //     $nama = "Vina";
+    //     return "halo $nama";
+    // }
+
+    // echo $nama;
+
+    // var global
+    // $nama = "Vina";
+
+    // var lokal
+   
+    // function cetakNama($x){
+    //     return "Halo $x";
+    // }
+    // echo cetakNama('Vina');
+
+    // // argumen
+    // echo cetakNama($nama);
+
+    // $nama = '';
+
+    // echo $nama;
+
+    // SUPERGLOBALS
+    // Variabel PHP yang bisa kita pakai dimanapun
+    // bENTUKNYA ADALAH Array Associative = array yg indeksnya string
+    // $_GET
+    // $_POST
+    // $_REQUEST
+    // $_SERVER
+    // array gabisa di echo
+    // cara cetak array pake 
+    // var_dump ($_SERVER["SERVER_NAME"]);
+    // $_GET = [
+    //     "nama" => "Vina"
+    // ];
+    // $_GET["nama"] = "Vina";
+    // $_GET["email"] = "vina@gmail.com";
+    // echo $_GET["nama"]; 
+    var_dump($_GET);
+?>
---
+++ kuliah/pertemuan7/kuliah_latihan3.php
@@ -0,0 +1,4 @@
+<!-- <?php echo $_POST["nama"]; ?> -->
+
+<h1>Selamat Datang Admin!</h1>
+<a href="kuliah_latihan2.php">Kembali</a>
